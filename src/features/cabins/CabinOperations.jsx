d0a60ac0b2541a8f29d44658b081b8b6920b968b
HusseinSerag <117987162+HusseinSerag@users.noreply.github.com>
2024-03-07 21:04:39 +0200

    Refactor Sort for resuability

--- src/features/cabins/CabinOperations.jsx
+++ src/features/cabins/CabinOperations.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import Filters from "./Filters";
-import Sort from "./Sort";
+import Sort from "../../ui/Sort";
 
 const Container = styled.div`
   display: flex;
@@ -11,7 +11,17 @@ export default function CabinOperations() {
   return (
     <Container>
       <Filters />
-      <Sort />
+      <Sort
+        name="sort"
+        options={[
+          { value: "asc-name", label: "Sort by name (A-Z)" },
+          { value: "desc-name", label: "Sort by name (Z-A)" },
+          { value: "asc-price", label: "Sort by price (low first)" },
+          { value: "desc-price", label: "Sort by price (high first)" },
+          { value: "asc-cap", label: "Sort by capacity (low first)" },
+          { value: "desc-cap", label: "Sort by capacity (high first)" },
+        ]}
+      />
     </Container>
   );
 }
---
+++ src/ui/Sort.jsx
@@ -0,0 +1,31 @@
+import { useSearchParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Select = styled.select`
+  border: 1px solid var(--color-grey-100);
+  box-shadow: var(--box-shadow);
+  background-color: var(--color-grey-0);
+  padding: 1rem;
+  font-weight: 600;
+  border-radius: var(--border-radius-md);
+  font-size: 1.4rem;
+  width: 100%;
+`;
+
+export default function Sort({ options, name, ...props }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  function handleSort(value) {
+    searchParams.set(name, value);
+    setSearchParams(searchParams);
+  }
+  return (
+    <Select onChange={(e) => handleSort(e.target.value)} {...props}>
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </Select>
+  );
+}
